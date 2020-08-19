5c016639f217f452b818448c35c4f4fb294b2362
Andrey Kozel <andrey.kozel@kaseya.com>
2020-08-19 20:26:24 +0300

    Added matreial-ui to the project. A few pages were updated with material UI.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,42 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-import styles from './Header.module.scss';
+import { createStyles, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import ApplicantSwitcher from 'components/ApplicantSwitcher';
 
 type HeaderProps = {
   title: string;
 };
 
-const Header: FC<HeaderProps> = ({ title }) => (
-  <div className={styles.form__header}>
-    <h4 className={styles.form__header_title}>{title}</h4>
-    <Link to="/courseinformation">Список курсов</Link>
-  </div>
+const useStyles = makeStyles(() =>
+  createStyles({
+    header: {
+      backgroundColor: '#2371a5',
+      color: '#fff',
+      height: '100%',
+    },
+  }),
 );
+
+const Header: FC<HeaderProps> = ({ title }) => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  return (
+    <Grid item container xs justify="space-between" className={classes.header}>
+      <Grid item xs={9}>
+        <h4>{title}</h4>
+      </Grid>
+      <Grid item container xs>
+        <Grid item xs>
+          <Link to="/courseinformation">Список курсов</Link>
+        </Grid>
+        <ApplicantSwitcher pathName={location.pathname} />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Header;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,25 +1,51 @@
 import React, { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+
+import { createStyles, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Footer from 'components/Footer';
 import Header from 'components/Header';
-import ApplicantSwitcher from 'components/ApplicantSwitcher';
-
-import styles from './Main.module.scss';
 
 type MainProps = {
   children?: React.ReactElement<any>;
 };
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      minHeight: '100vh',
+      minWidth: '100%',
+    },
+    header: {
+      height: '7vh',
+    },
+    content: {
+      height: '86vh',
+      overflow: 'auto',
+      paddingTop: 10,
+      paddingBottom: 10,
+    },
+    footer: {
+      height: '7vh',
+    },
+  }),
+);
+
 const Main: FC<MainProps> = ({ children }) => {
-  const location = useLocation();
+  const classes = useStyles();
+
   return (
-    <>
-      <Header title="Free It" />
-      <ApplicantSwitcher pathName={location.pathname} />
-      <div className={styles.main}>{children}</div>
-      <Footer title="Free It Footer" />
-    </>
+    <Grid direction="column" xs className={classes.root}>
+      <Grid item className={classes.header}>
+        <Header title="Free It" />
+      </Grid>
+      <Grid item className={classes.content}>
+        {children}
+      </Grid>
+      <Grid item className={classes.header}>
+        <Footer title="Free It Footer" />
+      </Grid>
+    </Grid>
   );
 };
 
